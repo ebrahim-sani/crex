47b9903ca81615b339b5b20212225c38985737b7
ebrahim-sani <ibraheemsani035@gmail.com>
2023-02-06 14:22:09 +0100

    ...

--- app/dashboard/ChartView.tsx
+++ app/dashboard/ChartView.tsx
@@ -1,29 +1,9 @@
 "use client";
 import React from "react";
 import { Chart } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from "chart.js";
 
-import {
-   Chart as ChartJS,
-   CategoryScale,
-   LinearScale,
-   PointElement,
-   LineElement,
-   Title,
-   Tooltip,
-   Legend,
-   Filler,
-} from "chart.js";
-
-ChartJS.register(
-   Title,
-   Tooltip,
-   Legend,
-   Filler,
-   CategoryScale,
-   LinearScale,
-   PointElement,
-   LineElement,
-);
+ChartJS.register(...registerables);
 
 const ChartView = ({ dataSets, options }: any) => {
    return <Chart type="line" options={options} data={dataSets} />;
